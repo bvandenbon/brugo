7211988eb6166dc1712e896a18f435fc62d7cf19
Bram Vandenbon <bvandenbon@gmail.com>
2018-07-28 23:10:32 +0200

    released version 0.1.0

--- release.bat
+++ release.bat
@@ -1,1 +1,4 @@
-mvn install:install-file -DgroupId=brugo -DartifactId=brugo -Dversion=0.1 -Dfile=C:\Users\ba\IdeaProjects\brugo\target\brugo-0.1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true 
+mvn install:install-file -DgroupId=brugo -DartifactId=brugo -Dversion=0.1.0 -Dfile=C:\Users\ba\IdeaProjects\brugo\target\brugo-0.1.0.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true 
+git add -A .
+git commit -m "released version"
+git push origin repository
